a061e1947d10240f65ad3e34322b5eda76c97bca
Tai-Ku <116546516+Tai-Ku@users.noreply.github.com>
2023-06-09 16:24:42 -0700

    update-edit Users

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,4 @@
+import { json } from "body-parser";
 import pool from "../configs/connnectDB";
 
 let getHomePage = async (req, res) => {
@@ -19,8 +20,39 @@ let createNewUser = async (req, res) => {
   );
   return res.redirect("/");
 };
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("DELETE FROM users WHERE id = ?", [userId]);
+  return res.redirect("/");
+};
+
+let editUser = async (req, res) => {
+  let id = req.params.id;
+  const [user] = await pool.execute("select * from users where id = ?", [id]);
+
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+let updateUser = async (req, res) => {
+  const { firstName, lastName, email, address, id } = req.body;
+  console.log(firstName, lastName, email, address);
+  await pool.execute(
+    "update users set FirstName = ? , LastName = ? , email = ? , address = ? where id = ?",
+    [
+      firstName || nul,
+      lastName || null,
+      email || null,
+      address || null,
+      id || null,
+    ]
+  );
+
+  return res.redirect("/");
+};
 module.exports = {
   getHomePage,
   getDetail,
   createNewUser,
+  deleteUser,
+  editUser,
+  updateUser,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,9 @@ const initWebRoute = (app) => {
   router.get("/", route.getHomePage);
   router.get("/detail/user/:userId", route.getDetail);
   router.post("/create-new-user", route.createNewUser);
+  router.post("/delete-user", route.deleteUser);
+  router.get("/edit-user/:id", route.editUser);
+  router.post("/update-user", route.updateUser);
   router.get("/about", (req, res) => {
     res.send(`I'm Eric!`);
   });
