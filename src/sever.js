4de7bceaa92d0c258c1f31bb925903b55ee6ee59
Tai-Ku <116546516+Tai-Ku@users.noreply.github.com>
2023-06-12 13:40:12 -0700

    middleware

--- src/sever.js
+++ src/sever.js
@@ -7,14 +7,23 @@ import initAPIRoute from "./route/api";
 require("dotenv").config();
 
 const app = express();
+var morgan = require("morgan");
 const port = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  console.log(req.method);
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(morgan("combined"));
 configViewEngine(app);
 initWebRoute(app);
 initAPIRoute(app);
+// handle 404 not found
+app.use((req, res) => {
+  res.send("404 NOT FOUND");
+});
 
 app.listen(port, () => {
   console.log("server running 8080");
